09920ef659910d7c603569132ed9926afe85e5c4
Allan Soares Duarte <allan.duarte@gulf.com.br>
2020-01-07 18:21:12 -0300

    Add protein translation solution

---
+++ protein-translation/lib/protein_translation.ex
@@ -0,0 +1,80 @@
+defmodule ProteinTranslation do
+  @proteins %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    list =
+      rna
+      |> String.graphemes()
+      |> Enum.chunk_every(3)
+
+    proteins =
+      for x <- list do
+        codon = Enum.join(x)
+
+        tuple =
+          case of_codon(codon) do
+            {:error, _} -> {:error, "invalid RNA"}
+            {:ok, "STOP"} -> {:ok, "STOP"}
+            {:ok, protein} -> {:ok, protein}
+          end
+
+        {_, protein} = tuple
+
+        protein
+      end
+
+      Enum.uniq(proteins)
+      |> Enum.reject(fn x -> x == "STOP" end)
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    case Map.fetch(@proteins, codon) do
+      :error -> {:error, "invalid codon"}
+      {:ok, _} = value -> value
+    end
+  end
+end
