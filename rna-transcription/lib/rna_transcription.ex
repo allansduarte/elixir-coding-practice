bcb81cd7db959f4ab566c1df0da234a50b75b18b
Allan Soares Duarte <allan.duarte@gulf.com.br>
2020-01-16 11:42:47 -0300

    Add better solution to rna-transcription

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -9,23 +9,17 @@ defmodule RnaTranscription do
   """
 
   @rna_map %{
-    "G" => "C",
-    "C" => "G",
-    "T" => "A",
-    "A" => "U"
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U
   }
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    dna
-    |> String.graphemes()
-    |> Enum.map(&translate_to_rna/1)
-    |> Enum.join("")
+    Enum.map(dna, &translate_to_rna/1)
   end
 
   defp translate_to_rna(nucleotide) do
-    case Map.fetch(@rna_map, nucleotide) do
-      :error -> {:error, "invalid nucleotide"}
-      {:ok, result} -> result
-    end
+    Map.fetch!(@rna_map, nucleotide)
   end
 end
