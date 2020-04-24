1808dcb56a577dad1408def9d023d8a1ac3063d6
Allan <allan.sduarte@gmail.com>
2020-04-23 22:48:20 -0300

    feat(rumbl): Add socket tracking

--- others/rumbl/lib/rumbl/multimedia.ex
+++ others/rumbl/lib/rumbl/multimedia.ex
@@ -10,8 +10,27 @@ defmodule Rumbl.Multimedia do
   import Ecto.Query, warn: false
 
   alias Rumbl.Repo
-  alias Rumbl.Multimedia.{Video, Category}
+  alias Rumbl.Multimedia.Annotation
+  alias Rumbl.Multimedia.Category
+  alias Rumbl.Multimedia.Video
   alias Rumbl.Accounts
+
+  def annotate_video(%Accounts.User{id: user_id}, video_id, attrs) do
+    %Annotation{video_id: video_id, user_id: user_id}
+    |> Annotation.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def list_annotations(%Video{} = video, since_id \\ 0) do
+    Repo.all(
+      from(a in Ecto.assoc(video, :annotations),
+        where: a.id > ^since_id,
+        order_by: [asc: a.at, asc: a.id],
+        limit: 500,
+        preload: [:user]
+      )
+    )
+  end
 
   def list_videos do
     Repo.all(Video)
