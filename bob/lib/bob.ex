2500596de2c4e72726bc1eb37aa3cd45fd01afeb
Allan <allan.sduarte@gmail.com>
2020-05-03 18:59:38 -0300

    feat(exercism): Add Bob solution with String module only

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -7,25 +7,19 @@ defmodule Bob do
 
   defp do_hey(input) do
     cond do
-      is_shouting?(input) and is_question?(input) -> "Calm down, I know what I'm doing!"
-      is_question?(input) -> "Sure."
-      is_shouting?(input) -> "Whoa, chill out!"
-      is_empty?(input) -> "Fine. Be that way!"
+      loud_question?(input) -> "Calm down, I know what I'm doing!"
+      question?(input) -> "Sure."
+      shouting?(input) -> "Whoa, chill out!"
+      silence?(input) -> "Fine. Be that way!"
       true -> "Whatever."
     end
   end
 
-  defp is_shouting?(input) do
-    input
-    |> do_only_words()
-    |> is_yelling?()
-  end
+  defp shouting?(input),
+    do: input == String.upcase(input) and input != String.downcase(input)
 
-  defp is_yelling?(input),
-    do: (!is_empty?(input) and input == String.upcase(input)) or String.ends_with?(input, "!")
+  defp loud_question?(input), do: shouting?(input) and question?(input)
 
-  defp do_only_words(input), do: String.replace(input, ~r/[[:digit:][:punct:][:blank:]]/, "")
-
-  defp is_question?(input), do: String.ends_with?(input, "?")
-  defp is_empty?(input), do: input == ""
+  defp question?(input), do: String.ends_with?(input, "?")
+  defp silence?(input), do: input == ""
 end
