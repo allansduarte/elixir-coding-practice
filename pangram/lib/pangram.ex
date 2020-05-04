27f9c301a2b0fbc27c02d482c34d6f90a2af81e0
Allan <allan.sduarte@gmail.com>
2020-05-03 21:09:45 -0300

    feat(exercism): Add Pangram solution

---
+++ pangram/lib/pangram.ex
@@ -0,0 +1,28 @@
+defmodule Pangram do
+  @alphabet ?a..?z
+
+  @doc """
+  Determines if a word or sentence is a pangram.
+  A pangram is a sentence using every letter of the alphabet at least once.
+
+  Returns a boolean.
+
+    ## Examples
+
+      iex> Pangram.pangram?("the quick brown fox jumps over the lazy dog")
+      true
+
+  """
+
+  @spec pangram?(String.t()) :: boolean
+  def pangram?(sentence) do
+    sentence
+    |> String.downcase()
+    |> String.to_charlist()
+    |> do_pangram()
+  end
+
+  defp do_pangram(chars) do
+    Enum.all?(@alphabet, &(&1 in chars))
+  end
+end
