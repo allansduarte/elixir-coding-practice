b632eb60f4ba85ba90b9fc9b7cc24e115dd0839f
Allan <allan.sduarte@gmail.com>
2020-05-27 12:32:33 -0300

    Add List Ops solution

---
+++ list-ops/lib/list_ops.ex
@@ -0,0 +1,35 @@
+defmodule ListOps do
+  # Please don't use any external modules (especially List or Enum) in your
+  # implementation. The point of this exercise is to create these basic
+  # functions yourself. You may use basic Kernel functions (like `Kernel.+/2`
+  # for adding numbers), but please do not use Kernel functions for Lists like
+  # `++`, `--`, `hd`, `tl`, `in`, and `length`.
+
+  @spec count(list) :: non_neg_integer
+  def count([_head | tail]), do: 1 + count(tail)
+  def count([]), do: 0
+
+  @spec reverse(list) :: list
+  def reverse(l), do: reverse(l, [])
+  def reverse([head | tail], acc), do: reverse(tail, [head | acc])
+  def reverse([], acc), do: acc
+
+  @spec map(list, (any -> any)) :: list
+  def map(l, f), do: for(e <- l, do: f.(e))
+
+  @spec filter(list, (any -> as_boolean(term))) :: list
+  def filter(l, f), do: for(e <- l, f.(e), do: e)
+
+  @type acc :: any
+  @spec reduce(list, acc, (any, acc -> acc)) :: acc
+  def reduce([head | tail], acc, f), do: reduce(tail, f.(head, acc), f)
+  def reduce([], acc, _), do: acc
+
+  @spec append(list, list) :: list
+  def append([head | tail], b), do: [head | append(tail, b)]
+  def append([], b), do: b
+
+  @spec concat([[any]]) :: [any]
+  def concat([head | tail]), do: append(head, concat(tail))
+  def concat([]), do: []
+end
