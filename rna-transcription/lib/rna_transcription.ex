b3e93c2cca55a11ad3e00c0050e572b785ee4fec
Allan <allan.sduarte@gmail.com>
2020-10-29 15:42:52 -0300

    Change RNA Transcription solution

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -8,14 +8,18 @@ defmodule RnaTranscription do
   'UGAC'
   """
 
-  @rna %{
-    ?G => 'C',
-    ?C => 'G',
-    ?T => 'A',
-    ?A => 'U'
+  @rna_map %{
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U
   }
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    Enum.reduce(dna, [], fn x, acc -> acc ++ @rna[x] end)
+    Enum.map(dna, &translate_to_rna/1)
+  end
+
+  defp translate_to_rna(nucleotide) do
+    Map.fetch!(@rna_map, nucleotide)
   end
 end
