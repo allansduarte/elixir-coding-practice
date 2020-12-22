f313f4f4f72ba07721259c7b5b8b0d959bbefa52
Allan <allan.sduarte@gmail.com>
2020-12-22 19:04:58 -0300

    Change Prime Factors

--- prime-factors/lib/prime_factors.ex
+++ prime-factors/lib/prime_factors.ex
@@ -8,18 +8,15 @@ defmodule PrimeFactors do
   The prime factors of 'number' will be ordered lowest to highest.
   """
   @spec factors_for(pos_integer) :: [pos_integer]
+  def factors_for(1), do: []
+
+  def factors_for(number) when rem(number, 2) == 0, do: [2] ++ factors_for(div(number, 2))
+
   def factors_for(number) do
-    number
-    |> do_factors_for()
-    |> Enum.reverse()
+    factor =
+      3..(number |> :math.sqrt() |> floor())
+      |> Enum.find(number, &(rem(number, &1) == 0))
+
+    [factor] ++ factors_for(div(number, factor))
   end
-
-  def do_factors_for(number, factor \\ 2, acc \\ [])
-  def do_factors_for(1, _factor, acc), do: acc
-
-  def do_factors_for(number, factor, acc) when rem(number, factor) == 0 do
-    do_factors_for(div(number, factor), factor, [factor | acc])
-  end
-
-  def do_factors_for(number, factor, acc), do: do_factors_for(number, factor + 1, acc)
 end
